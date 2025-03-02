45aafc91cf455cd80b0ca10f480ec3c24ddfc9db
Yan <yan.postnov.main@gmail.com>
2025-03-02 19:04:05 +0200

    fix: remove route.ts

--- src/entities/survey/api.ts
+++ src/entities/survey/api.ts
@@ -2,12 +2,14 @@
 
 import { promises as fs } from 'fs'
 
+import { cache } from "react";
+
 import { Survey } from "@/entities/survey/types";
 
 // lets pretend we have feature flag or process.env
-export const loadSurvey = async (): Promise<Survey> => {
+export const loadSurvey = cache(async (): Promise<Survey> => {
     const survey = process.env.SURVEY ?? 'astrology';
     const file = await fs.readFile(process.cwd() + `/public/surveys/${survey}.json`, 'utf8');
 
     return JSON.parse(file) as Survey;
-}
+})
