f6a6fcf1a145ccf7ae094d59ffbd2703944f15dc
Yan <yan.postnov.main@gmail.com>
2025-03-02 17:28:58 +0200

    chore: update readme

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 [Deployed on Vercel](https://clario-login-form.vercel.app/)
 
 ## Installation:
-
+- `fill .env variables`
+```dotenv
+SURVEY=astrology
+NEXT_PUBLIC_URL=http://localhost:3000
+```
 - `npm i`
 - `npm run dev`
 
--- src/entities/survey/api.ts
+++ src/entities/survey/api.ts
@@ -3,7 +3,7 @@
 import { Survey } from "@/entities/survey/types";
 
 export const loadSurvey = async (): Promise<Survey> => {
-    const res = await fetch("http://localhost:3000/survey", {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/survey`, {
         cache: 'force-cache',
         next: { revalidate: 60 }
     });
